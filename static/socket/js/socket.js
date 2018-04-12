393a2ce3fe5b9de9850e5990aff5aa960bc3f3a5
chendd <869316456@qq.com>
2018-04-12 15:00:28 +0800

    端口修改

--- static/socket/js/socket.js
+++ static/socket/js/socket.js
@@ -52,7 +52,7 @@ $(function () {
             }
         })
     //生成 soket实例
-    var socket = io.connect("ws://39.107.236.45:8099");
+    var socket = io.connect("ws://120.27.212.237");
     //通知服务器有用户登录
     if (userInfo && userInfo.userName) {
         socket.emit('login', userInfo);
